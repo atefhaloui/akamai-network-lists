7056b24aba3d3a51ed0f0b8425aa82087e8a5cbb
atefhaloui <atef.haloui@gmail.com>
2022-03-25 21:58:33 +0100

    Use %s instead of %v when appropriate

--- api_test.go
+++ api_test.go
@@ -11,30 +11,49 @@ func TestNetworkListEndpoint_Add(t *testing.T) {
 	config, err := edgegrid.InitEdgeRc("./edgerc", "ccu")
 	config.Debug = false
 	if err != nil {
-		t.Fatalf("Cannot load config file: %v", err)
+		t.Fatalf("Cannot load config file: %s", err)
 	}
 
 	// Select Network list: IP Blacklist
-	nls, err := New(&config, "XXXXXX_IPBLACKLIST") // @todo: Update the ID
+	nls, err := New(&config, "XXXXX_IPBLACKLIST") // @todo: Update the ID
 	if err != nil {
-		t.Fatalf("Cannot access the network list: %v", err)
+		t.Fatalf("Cannot access the network list: %s", err)
 	}
 
 	if err := nls.Add("195.109.4.59/32"); err != nil {
-		t.Errorf("Add() error = %v", err)
+		t.Errorf("Add() error = %s", err)
 	}
 
 	if err := nls.Activate(Staging, "test only", nil); err != nil {
-		t.Errorf("Activate(Staging) error = %v", err)
+		t.Errorf("Activate(Staging) error = %s", err)
 	}
 
 	var status *string
 	required := "MODIFIED"
 	if status, err = nls.GetActivationStatus(Staging); err != nil {
-		t.Errorf("GetActivationStatus(Staging) error = %v", err)
+		t.Errorf("GetActivationStatus(Staging) error = %s", err)
 	}
 
 	if *status != required {
-		t.Errorf("Invalid status: found = %v, required = %v", *status, required)
+		t.Errorf("Invalid status: found = %s, required = %s", *status, required)
+	}
+}
+
+func TestNetworkListEndpoint_Remove_WithError(t *testing.T) {
+	// Load config
+	config, err := edgegrid.InitEdgeRc("./edgerc", "ccu")
+	config.Debug = false
+	if err != nil {
+		t.Fatalf("Cannot load config file: %s", err)
+	}
+
+	// Select Network list: IP Blacklist
+	nls, err := New(&config, "XXXXX_IPBLACKLIST") // @todo: Update the ID
+	if err != nil {
+		t.Fatalf("Cannot access the network list: %s", err)
+	}
+
+	if err := nls.Delete("1.2.3.4/32"); err == nil {
+		t.Errorf("Delete() didn't fail as expected")
 	}
 }
